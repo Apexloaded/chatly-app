058f26d94784abd68cbed12771a85b2dffc882a2
Apexloaded <86329875+Apexloaded@users.noreply.github.com>
2021-12-18 00:13:56 +0100

    next config file added

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    REACT_APP_MORALIS_APPLICATION_ID: 'RvvHhfkPKqohLKreyGm7pomiCyhnjifHvKUWGNzh',
+    REACT_APP_MORALIS_SERVER_URL: 'https://h8lvx6ahet8r.usemoralis.com:2053/server'
+  }
+}
